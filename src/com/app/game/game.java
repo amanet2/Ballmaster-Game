3d4862ca0fb4159d90b240da9edcb471e7d48258
Stallion <tonyjmstallion@gmail.com>
2025-05-10 20:05:54 -0500

    cam instance

--- src/com/app/game/game.java
+++ src/com/app/game/game.java
@@ -2,12 +2,14 @@ package com.app.game;
 import java.util.Arrays;
 import com.app.engine.settings;
 import com.app.engine.utilsImpl;
+import com.app.engine.cameraImpl;
 
 public class game {
     public static String[] launchArgs;
     static final utilsImpl utils = new utilsImpl();
+    static final cameraImpl cam = new cameraImpl();
     static final int roundToTestVal = 36;
-    static final int roundToTestNearest = 20;
+    static final int roundToTestNearest = 30;
 
     public static void main(String[] args) {
         launchArgs = args;

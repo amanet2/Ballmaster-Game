07ac9a7e0caf9d6c4adbe41e124382b7aa6d5d75
Stallion <tonyjmstallion@gmail.com>
2025-05-17 14:30:47 -0500

    new console sys

--- src/com/app/game/gameConsoleTest.java
+++ src/com/app/game/gameConsoleTest.java
@@ -1,26 +1,31 @@
 package com.app.game;
 
-import com.app.engine.cmd;
-import com.app.engine.console;
+import com.app.engine.consoleSystem;
+import com.app.engine.consoleSystem.gConsoleCommand;
+import com.app.engine.consoleSystem.gConsoleSystem;
 
 public class gameConsoleTest {
-    public static void test() {
-        console console = new console();
-        console.registerCmd("echo", new cmd() {
-            @Override
-            public String doCmd(String[] args) {
-                StringBuilder echoStr = new StringBuilder();
-                for(String tok : args) {
-                    echoStr.append(tok).append(" ");
-                }
-                System.out.printf("%s%n", echoStr);
-                return echoStr.toString();
-            }
-        });
+    static consoleSystem consoleSystem = new consoleSystem();
 
-        console.registerCmd("add", new cmd() {
+    public static void test() {
+        gConsoleSystem gConsoleSystem = consoleSystem. new gConsoleSystem();
+
+        gConsoleCommand gConsoleCommandEcho = consoleSystem. new gConsoleCommand() {
             @Override
-            public String doCmd(String[] args) {
+            public String doCommand(String[] args) {
+                StringBuilder echoStrBuilder = new StringBuilder();
+                for(String tok : args) {
+                    echoStrBuilder.append(" ").append(tok);
+                }
+                String echoString = echoStrBuilder.substring(1);
+                System.out.printf("%s%n", echoString);
+                return echoString;
+            }
+        };
+
+        gConsoleCommand gConsoleCommandAdd = consoleSystem. new gConsoleCommand() {
+            @Override
+            public String doCommand(String[] args) {
                 try {
                     return Integer.toString(Integer.parseInt(args[0]) + Integer.parseInt(args[1]));
                 }
@@ -29,12 +34,19 @@ public class gameConsoleTest {
                 }
                 return "null";
             }
-        });
+        };
 
-        console.readLine("echo I am echoing something from the console!");
-        String addCom = "add 2 2";
-        String addResult = console.readLine(addCom);
-        console.readLine(String.format("echo I am echoing the result of command '%s' -> %s", addCom, addResult));
+        gConsoleSystem.registerCmd("echo", gConsoleCommandEcho);
+        gConsoleSystem.registerCmd("add", gConsoleCommandAdd);
+
+        String echoStr = "echo I am echoing something from the console!";
+        String addStr = "add 2 2";
+
+        String addResult = gConsoleSystem.readLine(addStr);
+
+        String addResultEchoStr = String.format("echo I am echoing the result of command '%s' -> %s", addStr, addResult);
+
+        gConsoleSystem.readLine(echoStr);
+        gConsoleSystem.readLine(addResultEchoStr);
     }
-
 }

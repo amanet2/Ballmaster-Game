7ce1a3dd6d09e6e26142cd71fe5c799a0cc52f45
Stallion <tonyjmstallion@gmail.com>
2025-05-17 20:32:44 -0500

    new sprite sys

--- src/com/app/game/game.java
+++ src/com/app/game/game.java
@@ -4,38 +4,28 @@ import java.util.Arrays;
 import com.app.engine.settings;
 import com.app.engine.utils;
 import com.app.engine.camera;
-import com.app.engine.sprites;
 import com.app.engine.keyboard;
 import com.app.engine.dict;
 
 public class game {
-    static final utils utils = new utils();
     static final camera camera = new camera();
-    static final sprites sprites = new sprites();
     static final keyboard keyboard = new keyboard();
-
-    static int roundToTestVal = 36;
-    static int roundToTestNearest = 30;
 
     public static void main(String[] args) {
         System.out.printf("Started Game w/ scale %d, args: %s%n", settings.nativeScale, Arrays.toString(args));
 
         gameCVarTest.test();
+        gameSpriteTest.test();
         gameConsoleTest.test();
         gameSchedulerTest.test();
         gameFileSystemTest.test();
 
-        dict testDict = new dict("{foo=bar,bar=");
-
-        System.out.printf(
-                "Rounding %d to nearest %d: %d%n",
-                roundToTestVal,
-                roundToTestNearest,
-                utils.roundToNearest(roundToTestVal, roundToTestNearest)
-        );
+        System.out.printf("Rounding 36 to nearest 30: %d%n", utils.roundToNearest(36, 30));
+        System.out.printf("Rounding 66 to nearest 30: %d%n", utils.roundToNearest(66, 30));
         System.out.printf("Cam coords: %s%n", Arrays.toString(camera.getCoords()));
-        System.out.printf("Sprite for 'none': %s%n", sprites.getScaledImage("none", 0, 0));
         System.out.printf("Keyboard code for key a: %d%n", keyboard.getCodeForKey("a"));
+
+        dict testDict = new dict("{foo=bar,bar=");
         System.out.printf(
                 "Test state: %s. (keys: %s) (foo value: %s, bar value:%s)%n",
                 testDict,
---
+++ src/com/app/game/gameSpriteTest.java
@@ -0,0 +1,15 @@
+package com.app.game;
+
+import com.app.engine.spriteSystem;
+import com.app.engine.spriteSystem.gSprite;
+import com.app.engine.spriteSystem.gSpriteSystem;
+
+public class gameSpriteTest {
+    static spriteSystem spriteSystem = new spriteSystem();
+
+    public static void test() {
+        gSpriteSystem gSpriteSystem = spriteSystem. new gSpriteSystem();
+        gSprite testSprite = gSpriteSystem.getScaledSprite("data/player_pint_03.png", 300, 300);
+        System.out.println("Got sprite " + testSprite);
+    }
+}
